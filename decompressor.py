f526278e30ad1db9b23e91fcf207c87237852300
JiaqiWang <wangjiaqi1992@outlook.com>
2018-12-12 15:07:12 -0500

    fix show() problem

--- decompressor.py
+++ decompressor.py
@@ -10,6 +10,22 @@ this compressor support three quality level:  low (PSNR = 30), medium (PSNR = 40
 I use three corresponding luminance quantization matrix to support this three quality levels
 by default, the compressor uses medium quality
 """
+# # the luminance quantization matrix with low-quality: PSNR = 30
+# low_jpeg_lq_matrix = [[16, 11, 10, 16, 24, 40, 51, 61], [12, 12, 14, 19, 26, 58, 60, 55],
+#                       [14, 13, 16, 24, 40, 57, 69, 56], [14, 17, 22, 29, 51, 87, 80, 62],
+#                       [18, 22, 37, 56, 68, 109, 103, 77], [24, 35, 55, 64, 81, 104, 113, 92],
+#                       [49, 64, 78, 87, 103, 121, 120, 101], [72, 92, 95, 98, 112, 100, 103, 99]]
+# # the luminance quantization matrix with medium-quality: PSNR = 40
+# medium_jpeg_lq_matrix = [[2, 2, 3, 4, 5, 6, 8, 11], [2, 2, 2, 4, 5, 7, 9, 11],
+#                          [3, 2, 3, 5, 7, 9, 11, 12], [4, 4, 5, 7, 9, 11, 12, 12],
+#                          [5, 5, 7, 9, 11, 12, 12, 12], [6, 7, 9, 11, 12, 12, 12, 12],
+#                          [8, 9, 11, 12, 12, 12, 12, 12], [11, 12, 12, 12, 12, 12, 12, 12]]
+# # the luminance quantization matrix with high-quality: PSNR = 50
+# high_jpeg_lq_matrix = [[1, 1, 1, 1, 1, 1, 1, 2], [1, 1, 1, 1, 1, 1, 1, 2],
+#                        [1, 1, 1, 1, 1, 1, 2, 2], [1, 1, 1, 1, 1, 2, 2, 3],
+#                        [1, 1, 1, 1, 2, 2, 3, 3], [1, 1, 2, 2, 3, 3, 3, 3],
+#                        [1, 1, 2, 2, 3, 3, 3, 3], [2, 2, 2, 3, 3, 3, 3, 3]]
+
 # the luminance quantization matrix with low-quality: PSNR = 30
 low_jpeg_lq_matrix = [[16, 11, 10, 16, 24, 40, 51, 61], [12, 12, 14, 19, 26, 58, 60, 55],
                       [14, 13, 16, 24, 40, 57, 69, 56], [14, 17, 22, 29, 51, 87, 80, 62],
@@ -25,7 +41,6 @@ high_jpeg_lq_matrix = [[1, 1, 1, 1, 1, 1, 1, 2], [1, 1, 1, 1, 1, 1, 1, 2],
                        [1, 1, 1, 1, 1, 1, 2, 2], [1, 1, 1, 1, 1, 2, 2, 3],
                        [1, 1, 1, 1, 2, 2, 3, 3], [1, 1, 2, 2, 3, 3, 3, 3],
                        [1, 1, 2, 2, 3, 3, 3, 3], [2, 2, 2, 3, 3, 3, 3, 3]]
-
 
 class DeCompressor:
     def __init__(self, fname, mode, quality):
@@ -162,7 +177,6 @@ class DeCompressor:
         matrix = self.level_shift()
         img = Image.fromarray(np.uint8(matrix), 'L')
         img.save(self.fname + 'restore.bmp')
-        img.show()
 
 
 if __name__ == '__main__':
